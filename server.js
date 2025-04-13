51424d44fdac9f02be8738fd1f51678fa6108c13
roshansu <163381434+roshansu@users.noreply.github.com>
2025-04-13 15:59:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 
 
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080;
 
 app.use(cors({
   origin: ['https://stud-mu.vercel.app'],
